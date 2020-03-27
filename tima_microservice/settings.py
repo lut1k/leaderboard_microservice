c82e87e9b91a68866874975825e57d9f5b22c99d
lut1k <lut1k@tut.by>
2020-03-27 13:32:57 +0300

    Connected PostrgreSQL using environment variables.

--- tima_microservice/settings.py
+++ tima_microservice/settings.py
@@ -77,11 +77,11 @@ WSGI_APPLICATION = 'tima_microservice.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'microservice_leaderboard',
-        'USER': 'microservice_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
