3f9081d51796674774e8d714a60f54e0fdc72c4a
lut1k <lut1k@tut.by>
2020-03-27 19:34:13 +0300

    Implemented the first endpoint.

--- leaderboard/pagination.py
+++ leaderboard/pagination.py
@@ -7,12 +7,3 @@ class CustomPagination(pagination.PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 20
 
-    def get_paginated_response(self, data):
-        return Response({
-            'links': {
-                'next': self.get_next_link(),
-                'previous': self.get_previous_link()
-            },
-            'count': self.page.paginator.count,
-            'results': data,
-        })
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -8,3 +8,14 @@ class LeaderBoardAPIView(ListAPIView):
     queryset = LeaderBoard.objects.all()
     serializer_class = LeaderBoardSerializer
     pagination_class = CustomPagination
+
+    def get_queryset(self):
+        player_position = self.request.GET.get('position')
+        filter_field = self.request.GET.get('filter')
+        if filter_field == 'before':
+            queryset = LeaderBoard.objects.filter(position__lt=player_position)
+            return queryset
+        elif filter_field == 'after':
+            queryset = LeaderBoard.objects.filter(position__gt=player_position)
+            return queryset
+        return super(LeaderBoardAPIView, self).get_queryset()
